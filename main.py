3f49b17cbd4eb0b74066b5ee626b785f7d4f4e62
Basil Tkachenko <baziltkachenko@gmail.com>
2023-02-03 09:27:26 +0300

    main partially done

---
+++ main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+from utils.utils import (
+    load_model
+)
+
+
+def header():
+    st.set_page_config(
+        oage_title="Celebrities classifier",
+        layput="wide",
+    )
+
+    st.title('Celebrities classifier')
+    st.markdown("Pet-project on image classification")
+    st.markdown("**Title:**Celebrities classifier with help of SVM")
+    st.markdown("Prepared by Basil Tkachenko")
+
+    st.sidebar.markdown("[prject repo](https://github.com/Basilt69/celebrities_classifier)")
+
+
+def main():
+    header()
+
+    st.header("Celebrities classifier")
+    activity = st.radio(
+        "Please, choose your action:", (
+            "1. Upload image.",
+            "2. Photo"
+        ),
+        index=0
+    )[:1]
+
+    model = load_model()
+
+    if activity == "1":
+        image = upload_image()
+        if image and st.button("Start classification"):
+            with st.spinner("Processing ..."):
+                extract_multiple_videos_faces(video, model)
+
+    elif activity == "2.":
+        photo =
